5acdf283911b50d15daad4224db3ad8115b8195c
Kjartan Már Andersen <svidakjammi@gmail.com>
2023-01-30 20:22:29 +0000

    Completed project 'A look behind the scenes'

--- react/07-a-look-behind-the-scenes/src/App.js
+++ react/07-a-look-behind-the-scenes/src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useCallback, useMemo } from "react";
 
-import './App.css';
+import Button from "./components/UI/Button/Button";
+// import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
+
+import "./App.css";
+import DemoList from "./components/UI/Button/Demo/DemoList";
 
 function App() {
+  const [listTitle, setListTitle] = useState('My List')
+  // const [showParagraph, setShowParagraph] = useState(false);
+  // const [allowToggle, setAllowToggle] = useState(false);
+
+  // const toggleParagraphHandler = useCallback(() => {
+  //   if (allowToggle) {
+  //     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  //   }
+  // }, [allowToggle]);
+
+  // const allowToggleHandler = () => {
+  //   setAllowToggle((prevAllotToggle) => !prevAllotToggle); 
+  // };
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, []);
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Toggle Paragraph</Button>
+      {/* <Button onClick={allowToggleHandler}>Allow Toggling</Button> */}
     </div>
   );
 }
---
+++ react/07-a-look-behind-the-scenes/src/components/UI/Button/Demo/MyParagraph.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyParagraph = (props) => {
+  return (
+    <p>{props.children}</p>
+  )
+}
+
+export default MyParagraph
