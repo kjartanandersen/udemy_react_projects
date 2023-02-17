c23c50414b907f65391ef0c33e55202104c77324
Kjartan Már Andersen <svidakjammi@gmail.com>
2023-02-17 00:13:21 +0000

    finished module

--- nextjs/01-first-setup/first-setup/pages/index.js
+++ nextjs/01-first-setup/first-setup/pages/index.js
@@ -1,6 +1,43 @@
 import Link from "next/link";
+import { useRef, useState } from "react";
 
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, feedback: enteredFeedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  function loadFeedbackHandler(event) {
+    event.preventDefault();
+
+    fetch("/api/feedback")
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackItems(data.feedback)
+      });
+
+
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
@@ -9,8 +46,24 @@ function HomePage() {
           <Link href="/portfolio">Portfolio</Link>
         </li>
         <li>
-          <Link href='/clients'>Clients</Link>
+          <Link href="/clients">Clients</Link>
         </li>
+      </ul>
+      <form>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="email" rows="5" ref={feedbackInputRef} />
+        </div>
+        <button onClick={submitFormHandler}>Send Feedback</button>
+      </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load Feedback</button>
+      <ul>
+        {feedbackItems.map((item) => <li key={item.id}>{item.text}</li>)}
       </ul>
     </div>
   );
